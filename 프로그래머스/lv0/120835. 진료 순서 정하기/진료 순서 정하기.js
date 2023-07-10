eb6fdd6a3b9bfb46022afcd820176c8f7c344ef2
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-07-10 14:12:00 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -0,0 +1,13 @@
+// 2023 ECMA  toSorted 사용불가
+
+// const solution = (arr) => {
+//     const sortedArr = arr.toSorted();
+//     return arr.map(item => sortedArr.indexof(item) + 1)
+// }
+
+const solution = (arr) => {
+    const arrOrigin = arr.map(i => i);
+    const sortedArr = arr.sort((a,b) => b - a);
+    return arrOrigin.map(item => sortedArr.indexOf(item) + 1);
+    
+}
