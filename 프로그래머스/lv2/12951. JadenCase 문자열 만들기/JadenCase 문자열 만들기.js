ea51e31ae454ad99bd999971b57b8867a2c7b954
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-07-15 14:56:15 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,20 +1,19 @@
-/*
-* 제한조건을 잘 읽자
-* s 의 길이는 1 이상  200 이하
-* - s 의 길이가 1일때 s.split(" ")[1] 을 참조할 수 없다.
-* 마찬가지로 s.split(" ").map(item => {} ) 에서도 item의 길이를 확인해야한다. 
-*/
+//  1. 메서드의 반환값을 잘 알고있어야한다.
+//  2. 공백문자열 ""의 length는 0이다.
 
-const solution = (s) => {
-    if (s.length <= 1) {
-        return s.toUpperCase()
-    }
+// const solution = (s) => {
+//     if (s.length <= 1) {
+//         return s.toUpperCase()
+//     }
     
-    return s.split(" ").map(item =>{
-        if (item.length <= 1) {
-            return item.toUpperCase();
-        }
-        else { 
-            return item[0].toUpperCase() + item.slice(1,item.length).toLowerCase()
-        }
-    }).join(" ")};
+//     return s.split(" ").map(item =>{
+//         if (item.length <= 1) {
+//             return item.toUpperCase();
+//         }
+//         else { 
+//             return item[0].toUpperCase() + item.slice(1,item.length).toLowerCase()
+//         }
+//     }).join(" ")};
+
+// 만약 charAt() 메서드를 사용한다면, 없는 인덱스에 접근해도 오류가 나지않는다.
+const solution = (s) => s.split(" ").map(item => item.charAt(0).toUpperCase() + item.substring(1).toLowerCase()).join(" ")
