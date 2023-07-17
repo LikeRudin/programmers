720b39ba05585a1137bb0e7a2135071e6eb7e6bb
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-07-17 17:07:05 +0900

    Update 완주하지 못한 선수.js

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,12 @@
 const solution = (participant, completion) => {
     const pObject = {};
-    for (const name of participant) {
-        pObject[name] = (pObject[name])? pObject[name] + 1 : 1;
-    }
-
-    for (const name of completion) {
+    
+    participant.forEach(name => pObject[name] = (pObject[name])? pObject[name] + 1 : 1);
+    
+    completion.forEach(name => {
         pObject[name] -= 1;
-        if (pObject[name] < 1) {
-            delete pObject[name];
-        }
-    }
+        if (pObject[name] < 1) delete pObject[name];
+    });
     
     const answer = Object.keys(pObject)[0];
     return answer;
