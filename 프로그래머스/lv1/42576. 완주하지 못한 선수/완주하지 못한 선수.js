82c5f6631fd3e341ea8286fc0959cae060d086b4
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-07-17 17:01:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.28 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,26 +1,15 @@
-// Object[item] => item이 string으로 바뀌어 들어간다
-// 즉. 변수값으로 프로퍼티에 접근할때에는 항상 대괄호 표기법을[] 사용하자
-
-// 배열순회 두번=> 끔찍한 성능
 const solution = (participant, completion) => {
     const pObject = {};
     for (const name of participant) {
-        if (!pObject[name]) {
-            pObject[name] = 1;
-        } else {
-            pObject[name] += 1;
+        pObject[name] = (pObject[name])? pObject[name] + 1 : 1;
+    }
+
+    for (const name of completion) {
+        pObject[name] -= 1;
+        if (pObject[name] < 1) {
+            delete pObject[name];
         }
     }
-    
-    
-    for (const item of completion) {
-        pObject[item] -= 1;
-        if (pObject[item] === 0) {
-            delete pObject[item];
-        }
-    }
-    
- 
     
     const answer = Object.keys(pObject)[0];
     return answer;
