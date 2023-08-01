bc24d7252911fe3dbe28a78fce7474c6d0625599
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-01 14:01:33 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,3 +1,13 @@
 /* 정규표현식 string.match => 패턴에 맞는 값을 가진 배열을 반환
 **/
-const solution = (order) => String(order).match(/[369]/g)? String(order).match(/[369]/g).length : 0 
+
+/**
+const solution = (order) => String(order).match(/[369]/g).length || 0 
+
+match의 결과가 null 일경우, length 가 작동하지않는다.
+*/
+
+// const solution = (order) => String(order).match(/[369]/g)? String(order).match(/[369]/g).length : 0 
+
+
+const solution = (order) => String(order).split("").filter(x => ["3","6","9"].includes(x)).length
