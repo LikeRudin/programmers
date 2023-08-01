3a04aabb29105788494f2a7519a1c36496c297d4
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-01 14:23:51 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,8 +1,10 @@
-const solution = (my_string) => my_string.split("").reduce((acc, item) => {
-    if (acc.includes(item)) {
-        return acc
-    }
-    acc.push(item)
-    return acc
+// const solution = (my_string) => my_string.split("").reduce((acc, item) => {
+//     if (acc.includes(item)) {
+//         return acc
+//     }
+//     acc.push(item)
+//     return acc
     
-}, []).join("")
+// }, []).join("")
+
+const solution = (my_string) => [...new Set(my_string)].join("");
