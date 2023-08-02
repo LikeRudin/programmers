9798db4b9973194b7e0c9bc2722e3554e6efbe71
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-02 09:45:34 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,24 +1,8 @@
-const solution = (numbers) => {
-    const answer = []
-    const dict = {
-        "zero": "0",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9"
-    }
-    for (let i =0; i < numbers.length ;i++){
-        for (const word in dict){
-            if (word === numbers.substring(i, i + word.length)) {
-                answer.push(dict[word]);
-                i = i + word.length -1 ; 
-                break;
-            }
-        }
-    }
-    return Number(answer.join(""))}
+const solution = (numbers) => Number(numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (num) => {
+         const dict = {"zero": "0","one": "1",
+        "two": "2","three": "3",
+        "four": "4","five": "5",
+        "six": "6","seven": "7",
+        "eight": "8","nine": "9"}
+         return dict[num]
+}))
