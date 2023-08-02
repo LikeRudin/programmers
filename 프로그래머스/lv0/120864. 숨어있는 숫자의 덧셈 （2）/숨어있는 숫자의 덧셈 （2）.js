4424d0149c22736d5ddf45055eff20a0d4920944
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-02 10:30:11 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 1.79 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,1 +1,3 @@
-const solution = (my_string) => my_string.match(/[0-9]+/g)? my_string.match(/[0-9]+/g).map(x => Number(x)).reduce((acc,cur) =>  acc + cur, 0) : 0
+// const solution = (my_string) => my_string.match(/[0-9]+/g)? my_string.match(/[0-9]+/g).map(x => Number(x)).reduce((acc,cur) =>  acc + cur, 0) : 0
+
+const solution = (my_string) => my_string.split(/\D+/i).map(x => Number(x)).reduce((acc,cur) => isNaN(cur)? acc : acc + cur, 0)
