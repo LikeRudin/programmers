69b163b1c86af191e594d8784121a3e54c32c2f1
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-03 14:22:25 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,24 @@
+const solution = (my_string) => {
+    const tokenArray = my_string.split(" ").map(x => (x === "+" || x ==="-")? x : Number(x));
+    let stack = [];
+    
+    for (let i = 0; i < tokenArray.length; i++){
+        stack = [...stack, tokenArray[i]];
+        
+        if (stack.length === 3){
+            const right = stack.pop();
+            let sigma = 0;
+            switch(stack.pop()){
+                case "+":
+                    sigma = stack.pop() + right; 
+                    break;
+                case "-":
+                    sigma = stack.pop() - right;
+                    break;
+            }
+            stack = [...stack, sigma]
+        }
+    
+    }
+return stack[0]
+}
