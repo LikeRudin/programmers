82112343ac9cf8cfe3483d5b7a8f13def52067e9
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-05 15:03:22 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,2 +1,25 @@
+/*
+* return
+    - 양수 : b , a
+    - 음수 : a , b
+    - 0이면 그대로 둔다
+    
+    n - x 값의 절대값이 작을수록 더 n에가까운 수이다.
+    
+즉 Math.abs(n - a) - Math.abs(n - b) > 0 
+    b가 더가깝다 
+    b, a
+    Math.abs(n - a) - Math.abs(n - b) < 0
+    a 가 더 가깝다
+    a , b
+    
+    Math.abs(n - a) - Math.abs(n - b) === 0
+    
+    a와 b가 같은거리에 있다.
+    
+    이런경우에 한하여, 오름차순에 의해 정렬한다
+    return b - a
+*/
+
 const solution = (numlist, n) => numlist
-.sort((a,b) => (Math.abs(n - b) === Math.abs(n - a))? b - a : Math.abs(n - a) - Math.abs(n - b))
+.sort((a,b) => Math.abs(n - a) - Math.abs(n - b) || b - a )
