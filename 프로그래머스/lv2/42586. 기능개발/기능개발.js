698f403eb7dd89db4113725b90d7036299282fd6
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-09 16:29:52 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,8 +1,11 @@
- //acc 구성[완료필요일, 같이완료되는작업수, 배포기능수 스택, 마지막 인덱스]
 
 const solution = (progresses, speeds) => {
-    const remainDays = progresses.map((progress,index) => Math.ceil((100 - progress)/speeds[index]))
-   
+    const remainDays = progresses.map((progress,index) => Math.ceil((100 - progress)/speeds[index]));
+    const firstDeployment = remainDays[0];
+    const lastWorkFlag = remainDays.length -1;
+    let deploymentStack = [];
+    let count = 0;
+    
     const answer = remainDays.reduce((acc,item,index) => {
         if (acc[0] >= item){
             acc[1] += 1;
@@ -15,7 +18,7 @@ const solution = (progresses, speeds) => {
             acc[2] = [...acc[2], acc[1]];
         }
         return acc;
-    }, [ remainDays[0], 0, [], remainDays.length -1])[2]
+    }, [firstDeployment, count, deploymentStack, lastWorkFlag])[2]
     return answer
 }
                    
