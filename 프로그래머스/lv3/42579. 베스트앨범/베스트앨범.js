3052604d133e5d082874a6a2deffb12833d7dd68
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-09 13:44:07 +0900

    [level 3] Title: 베스트앨범, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -1,67 +1,67 @@
 /*
-* 1. 장르를 재생수가 많은 순서대로 나열한다.
-* 2. 장르내에서 재생수가 제일 큰 곡을 먼저재생
-* 3. 재생수가 일치한다면, plays 에서 인덱스가 빠른 곡을 먼저 재생
+* 0. 두개를 합친 배열을 구한다.
+* 1. 장르별 조회수합산을 구한다
+* 2. 조회수가 많은 순서대로 장르를 나열한 배열을 만든다
+* 3. 각 장르별로 2곡씩 고른다
+* 4. 모든 값을 넣은 배열을 반환한다.
 */
 
-
-
-const getGenresPlaysHash = (genres, plays) => {
-    const genresPlays = {}
-     plays.forEach((num, index) => {
-          genresPlays[genres[index]] = (genresPlays[genres[index]])? genresPlays[genres[index]] += num: genresPlays[genres[index]] = num;
-     })
-    return genresPlays;
+const getMergedArray = (genres, plays) => {
+    const genrePlayIndex = plays.map((num,index) => [genres[index], num,index])
+    return genrePlayIndex;
 }
 
-const getSortedGenres = (obj) => {
-    const genreArray = Object.entries(obj).sort((a,b)  => b[1] - a[1]);
-    return genreArray.map(x => x[0]); 
+const getSortedGenres = (arr) => {
+    const genreViewHash = {};
+    arr.forEach(item => {
+        const [genre, num, _] = item;
+        genreViewHash[genre] = (genreViewHash[genre])? genreViewHash[genre] += num: num;
+    });
     
+    const sortedGenres =Object.entries(genreViewHash).sort((a,b) => (b[1] - a[1])).map(x => {
+        const [genre, _ ]= x;
+        return genre;
+    });
+    return sortedGenres
 }
 
-const getSongListForGenre = (genres,plays, genre) => {
-    const indexPlaysHash = {}
-    plays.forEach((num, index) => {
-        if (genres[index] === genre) {
-            indexPlaysHash[index] = num
-        }});
-    return indexPlaysHash 
-    
+
+const getSongListOfOneGenre = (mergedArray, genre) => {
+    const trackForOneGenre = mergedArray.filter(record => record[0] === genre)
+    const playAndIndex = trackForOneGenre.map(x=> [x[1], x[2]]).sort((a, b) => (b[0] - a[0]));
+    return playAndIndex;
 }
 
-const getTopTwoKeys = (obj) => {
-    // 길이가 1일경우에 key를 바로 반환
-    const beforeSorted = Object.entries(obj);
-    if (beforeSorted.length ===1 ){
-        return [beforeSorted[0][0]]
+const getTopTwoSongs = (trackArray) => {
+    if (trackArray.length ===1) {
+        return [trackArray[0][1]]
     }
- 
-    const entries = beforeSorted.sort((a, b) => b[1] - a[1]); 
+    const max = trackArray[0][0];
+    const maxArray = trackArray.filter(item => item[0] === max).map(x => x[1]);
+    if(maxArray.length > 1) {
+        return maxArray.sort((a,b) => a - b).slice(0,2);
+    }
+
+    const second = trackArray[1][0];
+    const secondArray =  trackArray.filter(item => item[0] === second).map(x => x[1]);
+    if (secondArray.length > 1) {
+        secondArray.sort((a,b) => a- b);
+    }
+    return [maxArray[0], secondArray[0]]
     
-    const max = entries.filter(item=> item[1] === entries[0][1]).map(x => x[0]);
-    //최댓값 인덱스가 두개이상일경우
-    if (max.length > 1) {
-        return max.sort().slice(0,2)
-    } else if (max.length === 1 && entries.length > 1) {
-        // 두번째값을 갖는 인덱스가 2개이상일경우
-        const second = entries.filter(item=> item[1] === entries[1][1]).map(x => x[0]);
-        return [max[0], second[0]]
-    } // 그냥 처음순서대로 반환
-    return [entries[0][0], entries[1][0]]
 }
-
-
 
 const solution = (genres, plays) => {
-    const genresPlaysHash = getGenresPlaysHash(genres, plays);
-    const sortedGenres = getSortedGenres(genresPlaysHash)
+    const mergedArray = getMergedArray(genres,plays);
+    const sortedGenres = getSortedGenres(mergedArray);
     let answer = [];
-    for (const genre of sortedGenres) {
-        const genreHash = getSongListForGenre(genres,plays,genre);
-        const indexesOfSongs = getTopTwoKeys(genreHash).map(x => +x);
-        answer = [...answer, ...indexesOfSongs];
+    for (const genre of sortedGenres){
+        const songList = getSongListOfOneGenre(mergedArray, genre);
+        const songs = getTopTwoSongs(songList);
+        answer = [...answer, ...songs];
     }
-    return answer
-
+    return answer;
+    
+    
+    
 }
