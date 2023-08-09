d924e547d75e1ac723f0d067934dd81ece4bb507
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-09 16:49:24 +0900

    [level 2] Title: 올바른 괄호, Time: 3.11 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,10 @@
+const solution = (s) => {
+    let count = 0;
+    for (let i =0; i <s.length; i++){
+        (s[i] ==="(")? count++ : count--;
+        if(count < 0) {
+            return false;
+        }
+    }
+    return (!count)
+}
