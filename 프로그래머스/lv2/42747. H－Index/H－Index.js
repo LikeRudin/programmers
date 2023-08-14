e770e0b16c909b85dcce873ca83e91e47ed6d28e
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-14 14:50:10 +0900

    [level 2] Title: H-Index, Time: 3.22 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -5,27 +5,29 @@
 * 2.오름차순으로 정렬된 배열을 순회하며
 *   아래의 조건을 만족하는 값이 나올때마다 h-idex를 덮어써준다.
 *
+*   h-index의 후보는 자신을 포함해서 인용수가 자신의 인용수이상인 논문들이
+*   적어도 자신의 인용수만큼 존재해야한다
 * - 순회값 >= 뒤에남은 성분의 개수
-* 핵심 트릭은 h - index는 특정논문의 인용수 그 자체는 아니라는것이다.
-*    
+*   === 값  >= 배열의 길이 - 현재 인덱스 
 *
 * 3.h 인덱스값을 반환한다.
 *   */
 
 const sortNumbersArrayByASC = (arr) => {
-    return arr.sort((a,b) => a - b)
+    return arr.sort((a,b) => b-a)
 }
 const calculateHindex = (arr) => {
-    let hIndex = [];
-    arr.every((paper, index) => {
+    let hIndex = 0;
+    arr.forEach((paper, index) => {
              const largerCitations = arr.length - index;
-                 hIndex = [...hIndex, Math.min(paper, largerCitations)]
-    return true});
-    return Math.max(...hIndex)
+             if (largerCitations >= paper) {
+                 hIndex = paper;
+             }});
+    return hIndex
 }
+
 const solution = (citations) => {
-    const sortedCitationsByASC = sortNumbersArrayByASC(citations);
-    console.log(sortedCitationsByASC);
-    const hIndex = calculateHindex(sortedCitationsByASC);
+    const sortedCitations = sortNumbersArrayByASC(citations);
+    const hIndex = calculateHindex(sortedCitations);
     return hIndex;
 }
