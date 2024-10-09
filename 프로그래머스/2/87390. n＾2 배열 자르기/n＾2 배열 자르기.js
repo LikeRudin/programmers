fd60823dd5718672893fe30b108930d373b4f16d
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2024-10-09 22:12:08 +0900

    [level 2] Title: n^2 배열 자르기, Time: 14.22 ms, Memory: 44.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,12 @@
+/*
+* 문제 예시대로 순차적으로 만들면 공간복잡도 에러가 난다.
+* 
+* 현재 위치를 n으로 나눠서 몫으로 행번호를 구하고, 
+* 나머지로 열번호를 구한다.
+* 
+*
+* 둘중 큰 값이 배열의 원소가된다.
+*/
+
+const solution = (n, left, right)=> Array.from({ length: right - left + 1 }, (_, i) => Math.max(Math.floor((left + i)/n)+1, (left + i) % n + 1));
+    
