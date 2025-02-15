79df02f94c0239fe592b2accee630da90f8cc085
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2025-02-16 06:46:19 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 104 ms, Memory: 9692 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const stdin = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const N = stdin[0];
+
+
+const BLANK = ' ';
+const STAR = '*';
+
+const starNumbers = Array.from({length: 2 * N - 1}, (_, i) => 
+    2 * (N - Math.abs(N - i - 1)) - 1
+);
+
+const lines = starNumbers.map(num => {
+    const blankNumbers = (2 * N - 1 - num) / 2;
+    return BLANK.repeat(blankNumbers) + STAR.repeat(num)
+});
+
+lines.forEach(line=> {console.log(line)})
