1d43c57d9f7ed5519ea61e8afe7a684edde50cb9
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2025-11-29 18:27:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,7 +1,1 @@
-const solution = (numbers) => {
-  const set = new Set(numbers)
-  return [1,2,3,4,5,6,7,8,9].reduce(
-    (sum, n) => set.has(n) ? sum : sum + n,
-    0
-  )
-}
+const solution = (numbers) => 45 - numbers.reduce((acc,cur)=> acc+cur,0)
